9ee6e74fa3617cd1c98d748d4c391aa0e86b628f
Mateusz Szmytko <mateusz.szmytko@gmail.com>
2017-06-13 02:38:04 +0200

    init

---
+++ dev/main.ts
@@ -0,0 +1,24 @@
+import {Query as q} from 'typeQuery/query/query';
+import { Init } from 'typeQuery/core/init';
+import {_App} from 'typeQuery/core/decorators/app.decorator';
+
+//import main styles
+import './styles/main.scss';
+
+@_App({
+    mixins: [],
+}) 
+class App {
+    constructor() { }
+    onInit() {
+        
+    }
+    
+}
+
+Init(App);
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "/main.min.css",
+});
+
+const uglifyJs = new webpack.optimize.UglifyJsPlugin({
+    include: /\.min\.js$/,
+    minimize: true,
+    sourceMap: true,
+    mangle: {
+        keep_fnames: true
+    }
+});
+const webpackConfig = {
+    entry: {
+        app: ['./dev/main.ts', 'es6-promise-polyfill']
+    }, 
+    devtool: "source-map",
+    output: {
+        path: __dirname + "/build",
+        filename: "[name].min.js"
+    },
+	resolve: {
+		alias: {
+			 typeQuery: path.resolve(__dirname, './node_modules/typeQuery/src/')
+		},
+        extensions: [".ts", ".js", ".json", ".css"]
+	},
+    module: {
+        rules: [
+            { test: /\.ts?$/, loader: "awesome-typescript-loader" },
+            { test: /\.css$/, use: extractSass.extract("css-loader?sourceMap&importLoaders=1") },
+            { test: /\.scss$/, use: extractSass.extract("css-loader?sourceMap&importLoaders=1!sass-loader?sourceMap&importLoaders=1") },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+	plugins: [
+        uglifyJs,
+        extractSass
+    ],
+    devServer: {
+        contentBase: './build',
+        inline: true,
+        open: true
+    }
+}
+
+module.exports = webpackConfig;
